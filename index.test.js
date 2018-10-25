a29b8adb1374dcc48b6348ffda776569d7963567
Eric Delerue <me@ericdelerue.com>
2018-10-25 12:41:12 +0200

    Code indentation

--- index.test.js
+++ index.test.js
@@ -12,9 +12,9 @@ const filterPositionsByCurrency = (positions, cur) => {
     positions.sort();
     let filtered_positions = {};
 		
-		filtered_positions = positions.filter( position => cur !== null ? position.currency == cur : position.currency !== '' );
+    filtered_positions = positions.filter( position => cur !== null ? position.currency == cur : position.currency !== '' );
 		
-		return filtered_positions;    
+    return filtered_positions;    
     
 }
 
@@ -23,43 +23,42 @@ const joinPortfoliosPositionsByIds = (portfolios, positions, cur) => {
     portfolios.sort();
     
     const filtered_positions = (cur !== null) ? filterPositionsByCurrency(positions, cur) : positions;
-		logger.info('filtered_positions: ', filtered_positions);
+    logger.info('filtered_positions: ', filtered_positions);
 		
-		for (let i in portfolios) {
-				let idPortefeuillesCourant = portfolios[i].id;
-				portfolios[i].portfoglios = filtered_positions.filter( position => position.portfolioId == idPortefeuillesCourant );
-		}
+    for (let i in portfolios) {
+	let idPortefeuillesCourant = portfolios[i].id;
+	portfolios[i].portfoglios = filtered_positions.filter( position => position.portfolioId == idPortefeuillesCourant );
+    }
 		
-		return portfolios;
+    return portfolios;
+
 };
 
 
 const getData = async function () {
 	
-	try {
-		
-		const currency = (arguments.length) ? arguments[0].toUpperCase() : null;
+    try {
 
-		// Connect to the database
-		const connection = await db.connect(true);
-		logger.info('connection: ', connection);
+        const currency = (arguments.length) ? arguments[0].toUpperCase() : null;
+
+        // Connect to the database
+        const connection = await db.connect(true);
+        logger.info('connection: ', connection);
 		
-		// Get the data
-		const {portfolios, positions} = await connection.load();
+        // Get the data
+        const {portfolios, positions} = await connection.load();
 		
-		// Join results and filter it
-		const filtered_results = joinPortfoliosPositionsByIds(portfolios, positions, currency);
+        // Join results and filter it
+        const filtered_results = joinPortfoliosPositionsByIds(portfolios, positions, currency);
+        logger.info('filtered_results: ', filtered_results);
 		
+        // Send it back 
+        return filtered_results;
 		
-		logger.info('filtered_results: ', filtered_results);
+    } catch(e) {
 		
-		// Send it back 
-		return filtered_results;
-		
-	} catch(e) {
-		
-    logger.info('e: ', e);
-    return null;
+      logger.error('e: ', e);
+      return null;
     
   }
   
