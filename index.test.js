4dd4c37520df597c7757013b4bd5a27c1a1834d3
Eric Delerue <me@ericdelerue.com>
2018-10-28 17:33:41 +0100

    Results filter improved

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@
 - Filter it
   Two-stage pass:
   1) combine the portfolios with their positions, and 
-  2) filter out those portfolios who have positions in the specified language.
+  2) filter out those portfolios who have positions in the specified currency.
 - Send it back 
 */
 const db = require('./database/');
@@ -19,7 +19,7 @@ function _findPositionsByPortfolios(portfolios, positions, currency) {
     return { ...portfolio, positions: filteredPositions };
   });
 
-  // If a language has been specified, return only those portfolios who have positions in that language
+  // If a currency has been specified, return only those portfolios who have positions in that currency
   return currency ? combined.filter(portfolio => portfolio.positions.some(position => position.currency === currency)) : combined;
   
 }
@@ -35,7 +35,7 @@ function findPositionsByPortfolios(portfolios, positions, currency) {
     return { ...portfolio, positions: filteredPositions };
   });
 
-  // If a language has been specified, return only those portfolios who have positions in that language
+  // If a currency has been specified, return only those portfolios who have positions in that currency
   return currency ? combined.filter(portfolio => portfolio.positions.some(position => position.currency === currency)) : combined;
   
 }
